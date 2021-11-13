a0b76dea5166c4036d6c4330c6172036e7e6c804
none <43687049+hjkbnma@users.noreply.github.com>
2021-11-13 18:36:51 -0500

    Updated main to have imports

--- src/main.py
+++ src/main.py
@@ -1,1 +1,37 @@
-# wut up Ravi
+# import
+
+
+import keras
+from keras.models import Sequential
+from keras.layers import Dense, Conv2D, MaxPool2D, Flatten, Dropout
+
+from keras.preprocessing.image import ImageDataGenerator
+from keras.optimizers import adam_v2
+
+from sklearn.metrics import classification_report, confusion_matrix
+
+import tensorflow as tf
+
+import cv2
+
+import os
+
+import numpy as np
+
+import matplotlib.pylab as plt
+
+labels = ['Red Maple', 'Black Oak']
+img_size = 224
+def get_data(data_dir):
+    data = [] 
+    for label in labels: 
+        path = os.path.join(data_dir, label)
+        class_num = labels.index(label)
+        for img in os.listdir(path):
+            try:
+                img_arr = cv2.imread(os.path.join(path, img))[...,::-1] #convert BGR to RGB format
+                resized_arr = cv2.resize(img_arr, (img_size, img_size)) # Reshaping images to preferred size
+                data.append([resized_arr, class_num])
+            except Exception as e:
+                print(e)
+    return np.array(data)
